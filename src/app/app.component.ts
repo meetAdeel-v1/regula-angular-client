fecb1a21ff8e7eb24244266bfc37de0b42be8788
Adeel Ahmed <adeel.ahmed@jubaexpress.com>
2023-09-21 15:53:56 +0400

    project-setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FaceLivenessDetailType } from '@regulaforensics/vp-frontend-face-components';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'regula-angular-client';
+  isOpen: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  faceLivenessHandler(data: CustomEvent<FaceLivenessDetailType>) {
+    if (data.detail.action === 'PROCESS_FINISHED') {
+        if (data.detail.data?.status === 1 && data.detail.data.response) {
+            console.log(data.detail.data.response);
+        }
+    }
+
+    if (data.detail?.action === 'CLOSE') {
+        this.isOpen = false;
+    }
+}
 }
